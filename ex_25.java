e8063fb13905da1b0f3548e1378c24ec0ae7eb17
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-02-08 17:32:21 -0300

    Término da resolução dos exercícios

--- ex_25.java
+++ ex_25.java
@@ -5,5 +5,21 @@ public class ex_25 {
      */
     public static void main(String[] args) {
 
-    }
-}
+        int base = 5, linha = 1;
+
+           do {
+               int num = 1, coluna = 1;
+               do {
+                   System.out.print(num + " ");
+                   num++;
+                   coluna++;
+               } while (coluna <= linha);
+
+               System.out.println();
+               linha++;
+                } while (linha <= base);
+            }
+
+        }
+
+
